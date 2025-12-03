1dee2a88c9606bc070b97638b1f76e5bf9864784
komangdiva <gedediva09@gmail.com>
2025-12-03 16:43:58 +0800

    membuat struktur awal halaman create

---
+++ create.php
@@ -0,0 +1,16 @@
+<?php
+    require_once __DIR__ . '/inc/config.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tambah Mahasiswa</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+        <h2>Tambah Mahasiswa</h2>
+
+    </body>
