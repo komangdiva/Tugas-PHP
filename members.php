629eef7c16ab361e3e12e574043431463fd20240
komangdiva <gedediva09@gmail.com>
2025-12-03 16:14:39 +0800

    membuat halaman members dengan html

---
+++ members.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Daftar Mahasiswa</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+<body>
+
+<h2>Daftar Mahasiswa</h2>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Prodi</th>
+            <th>Angkatan</th>
+            <th>Foto</th>
+            <th>Status</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+</table>
+</body>
+</html>
