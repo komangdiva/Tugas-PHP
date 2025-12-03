28c03c373176178dc13ca40b5f93d82556f5603f
komangdiva <gedediva09@gmail.com>
2025-12-03 18:12:23 +0800

    menambahkan html dan navigasi pada halaman edit

--- edit.php
+++ edit.php
@@ -13,3 +13,17 @@ $row = $mhs->getById((int)$id);
 if (!$row) {
     Utility::redirect('members.php', 'Data tidak ditemukan.');
 }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Edit Mahasiswa</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <?php Utility::showNav(); ?>
+
+        <h2>Edit Mahasiswa</h2>
+    </body>
+</html>
