bc0693b11fd8a1e8d8b3381855b75bc6f97f9e5a
komangdiva <gedediva09@gmail.com>
2025-12-03 14:10:19 +0800

    membuat dasar koonfigurasi seperti session, autoload, dan mensetting database

---
+++ inc/config.php
@@ -0,0 +1,23 @@
+<?php
+
+    session_start();
+
+    spl_autoload_register(function ($class_name){
+        $file = __DIR__ . '/../class/' . $class_name . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    });
+
+    const DB_HOST = 'localhost';
+    const DB_USER = 'root';
+    const DB_PASS = '';
+    const DB_NAME = 'tugas_php';
+
+    const BASE_URL = 'http://localhost:8000/';
+
+    const NAV_PAGES = [
+    ['title' => 'Home',   'url' => 'index.php'],
+    ['title' => 'Daftar', 'url' => 'members.php'],
+    ['title' => 'Tambah', 'url' => 'create.php']
+    ];
