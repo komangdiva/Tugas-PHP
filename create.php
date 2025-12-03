c161e15b5d76e3b87086fda87317885dad699045
komangdiva <gedediva09@gmail.com>
2025-12-03 16:52:25 +0800

    menambahkan input nama, nim, prodi, angkatan, status, dan upload foto pada form

--- create.php
+++ create.php
@@ -13,4 +13,35 @@
     <body>
         <h2>Tambah Mahasiswa</h2>
 
+        <form action="save.php" method="post" enctype="multipart/form-data">
+
+            <label>Nama</label>
+            <input type="text" name="nama" required>
+
+            <label>NIM</label>
+            <input type="text" name="nim" required>
+
+            <label>Prodi</label>
+            <select name="prodi" required>
+                <option value="">-- Pilih Prodi --</option>
+                <option value="SI">SI</option>
+                <option value="TI">TI</option>
+                <option value="MI">MI</option>
+                <option value="DKV">DKV</option>
+            </select>
+
+            <label>Angkatan</label>
+            <input type="number" name="angkatan" min="2000" max="2100" required>
+
+            <label>Status</label>
+            <select name="status" required>
+                <option value="aktif">Aktif</option>
+                <option value="tidak_aktif">Tidak Aktif</option>
+            </select>
+
+
+            <button type="submit" class="btn">Simpan</button>
+        </form>
+
     </body>
+</html>
