985370098bec57272d4584add2c97164bbf7c8b7
komangdiva <gedediva09@gmail.com>
2025-12-03 15:51:16 +0800

    menambahkan fungsi getall, getbyid, insert, update, delete

--- class/Mahasiswa.php
+++ class/Mahasiswa.php
@@ -5,4 +5,66 @@
         public function __construct() {
         $this->db = new Database();
         }
+
+        public function getAll() {
+            $sql = "SELECT * FROM mahasiswa ORDER BY id DESC";
+            $stmt = $this->db->query($sql);
+            return $stmt->fetchAll();
+        }
+
+         public function getById($id) {
+            $sql = "SELECT * FROM mahasiswa WHERE id = :id LIMIT 1";
+            $stmt = $this->db->query($sql, ['id' => $id]);
+            return $stmt->fetch();
+        }
+
+         public function insert($data) {
+            $sql = "INSERT INTO mahasiswa (nama, nim, prodi, angkatan, foto_path, status, created_at)
+                VALUES (:nama, :nim, :prodi, :angkatan, :foto_path, :status, NOW())";
+
+            return $this->db->query($sql, [
+                'nama'      => $data['nama'],
+                'nim'       => $data['nim'],
+                'prodi'     => $data['prodi'],
+                'angkatan'  => $data['angkatan'],
+                'foto_path' => $data['foto_path'],
+                'status'    => $data['status']
+            ]);
+        }
+
+        public function update($id, $data) {
+            $sql = "UPDATE mahasiswa 
+                SET nama = :nama,
+                    nim = :nim,
+                    prodi = :prodi,
+                    angkatan = :angkatan,
+                    foto_path = :foto_path,
+                    status = :status,
+                    updated_at = NOW()
+                WHERE id = :id";
+
+            return $this->db->query($sql, [
+                'nama'      => $data['nama'],
+                'nim'       => $data['nim'],
+                'prodi'     => $data['prodi'],
+                'angkatan'  => $data['angkatan'],
+                'foto_path' => $data['foto_path'],
+                'status'    => $data['status'],
+                'id'        => $id
+            ]);
+        }
+
+        public function delete($id) {
+            $old = $this->getById($id);
+
+            if (!empty($old['foto_path'])) {
+            $file = __DIR__ . '/../' . $old['foto_path'];
+                if (file_exists($file)) {
+                    @unlink($file);
+                }
+            }
+
+            $sql = "DELETE FROM mahasiswa WHERE id = :id";
+            return $this->db->query($sql, ['id' => $id]);
+        }
     }
