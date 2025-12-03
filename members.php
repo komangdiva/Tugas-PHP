1af5448397e50355e4490160044939a58334d762
komangdiva <gedediva09@gmail.com>
2025-12-03 16:33:57 +0800

    menambahkan aksi create, edit, dan hapus serta menampilkan data mahasiswa dalam tabel dengan menggunakan foreach

--- members.php
+++ members.php
@@ -19,6 +19,8 @@
 
 <?php Utility::showFlash(); ?>
 
+<a href="create.php" class="btn">+ Tambah Mahasiswa</a>
+
 <table>
     <thead>
         <tr>
@@ -33,6 +35,35 @@
         </tr>
     </thead>
     <tbody>
+        <?php if (empty($data)): ?>
+        <tr>
+            <td colspan="8">Belum ada data.</td>
+        </tr>
+        <?php else: ?>
+        <?php foreach ($data as $row): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($row['id']); ?></td>
+                <td><?php echo htmlspecialchars($row['nama']); ?></td>
+                <td><?php echo htmlspecialchars($row['nim']); ?></td>
+                <td><?php echo htmlspecialchars($row['prodi']); ?></td>
+                <td><?php echo htmlspecialchars($row['angkatan']); ?></td>
+                <td>
+                    <?php if (!empty($row['foto_path'])): ?>
+                        <img src="<?php echo htmlspecialchars($row['foto_path']); ?>" alt="foto" style="width:60px;height:auto;">
+                    <?php endif; ?>
+                </td>
+                <td><?php echo htmlspecialchars($row['status']); ?></td>
+                <td>
+                    <a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a> |
+                    <form action="delete.php" method="post" style="display:inline;" onsubmit="return confirm('Yakin hapus data ini?');">
+                        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                        <button type="submit" class="btn btn-danger">Hapus</button>
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    <?php endif; ?>
+    </tbody>
 </table>
 </body>
 </html>
